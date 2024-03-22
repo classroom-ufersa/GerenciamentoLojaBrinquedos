925b8a2353d1fef209afa10c9bf1d12ad4036bb4
Difierro <gustavo7867grdr@gmail.com>
2024-03-22 10:23:21 -0300

    test: add test file for brinquedos.c functions

---
+++ test/brinquedos_test.c
@@ -0,0 +1,42 @@
+#include "../include/brinquedos.h"
+#include "../include/sessao.h"
+
+struct brinquedo{
+    char nome[25];
+    int idadeMax;
+    int idadeMin;
+    float preco;
+    int qtdEstoque;
+    int idSessao;
+};
+
+int main(void){
+    int qtdBrinquedos = 0;
+
+    Brinquedo * brinquedos = contaBrinquedos(&qtdBrinquedos);
+    if (brinquedos == NULL){
+        printf("Falha na alocação de memória.\n");
+        exit(1);
+    }
+    preencheDados(brinquedos, qtdBrinquedos);
+    printf("primeiro: %d brinq\n", qtdBrinquedos);
+    for (int i = 0; i < qtdBrinquedos; i++){
+        printf("%s\t%d-%d\t%.2f\t%d\t%d\n", brinquedos[i].nome, brinquedos[i].idadeMin, brinquedos[i].idadeMax, brinquedos[i].preco, brinquedos[i].qtdEstoque, brinquedos[i].idSessao);
+    }
+    
+    addBrinquedo(&qtdBrinquedos);
+
+    brinquedos = realloc(brinquedos, qtdBrinquedos * sizeof(Brinquedo));
+    if (brinquedos == NULL){
+        printf("Falha na alocação de memória.\n");
+        exit(1);
+    }
+    preencheDados(brinquedos, qtdBrinquedos);
+    printf("segudo: %d brinq\n", qtdBrinquedos);
+    for (int i = 0; i < qtdBrinquedos; i++){
+        printf("%s\t%d-%d\t%.2f\t%d\t%d\n", brinquedos[i].nome, brinquedos[i].idadeMin, brinquedos[i].idadeMax, brinquedos[i].preco, brinquedos[i].qtdEstoque, brinquedos[i].idSessao);
+    }
+    
+    free(brinquedos);
+    return 0;
+}

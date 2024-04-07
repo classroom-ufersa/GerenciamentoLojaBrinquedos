b42a32212026ae7d71e8b4e67fd03181286311bd
euduar-da <mdasilvasouzs@gmail.com>
2024-04-07 18:33:57 -0300

    refactor: reestruturação do código

--- include/sessao.h
+++ include/sessao.h
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "../include/brinquedos.h"
 
 typedef struct sessao Sessao;
 
 typedef struct lista Lista;
+
+Sessao * criaListaSessao(void);
+
+Sessao * preencheNo(Sessao * sessoes, Sessao * aux);
+
+void contaSessoes(int * qtdSessoes);
+
+Sessao * preencheListaSessoes(Sessao * sessoes, int * qtdSessao);
+
+void imprimeSessao(Sessao * sessoes);
--- src/sessao.c
+++ src/sessao.c
@@ -17,36 +17,79 @@ struct brinquedo{
     char categoriaSessao[25];
 };
 
-void criaLista(void){
+Sessao * criaListaSessao(void){
     return NULL;
 }
 
-Sessao * preencheNo(Sessao * l, Brinquedo * brinquedo){
+Sessao * preencheNo(Sessao * sessoes, Sessao * aux){
     Sessao *novo = (Sessao*) malloc(sizeof(Sessao));
-    novo->prox = l;
-    novo->ant = NULL;
-    l->ant = novo;
-    novo->id = brinquedo->idSessao;
-    strcpy(novo->categoria, brinquedo->categoriaSessao);
-    
-    return novo;
-}
+    novo->id = aux->id;
+    strcpy(novo->categoria, aux->categoria);
 
-void escolheSessao(Sessao * l){
-    printf("As seguintes sessões estão cadastradas:");
-    Sessao* p;
-    for(p = l; p!= NULL; p = p->prox){
-        printf("Dados: %d%s\n", p->id, p->categoria);
+    if(sessoes == NULL) {
+        novo->prox = novo;
+        novo->ant = novo;
+    } else {
+        Sessao *ultimoNo = sessoes->ant;
+
+        novo->prox = sessoes;
+        sessoes->ant = novo;
+
+        novo->ant = ultimoNo;
+        ultimoNo->prox = novo;
     }
+    
+    return novo;    
 }
 
-void imprimeSessao(Brinquedo * brinquedos, int sessaoDesejada, int qtdBrinquedos){
-    int i;
-    for(i=0; i<sessaoDesejada; i++){
-        if(sessaoDesejada == brinquedos[i].idSessao){
-            printf("%s\t%d-%d\t%.2f\t%d\t%d\n", brinquedos[i].nome, brinquedos[i].idadeMin, brinquedos[i].idadeMax, brinquedos[i].preco, brinquedos[i].qtdEstoque, brinquedos[i].idSessao);
+void contaSessoes(int * qtdSessoes){
+    *qtdSessoes = 0;
+    int c;
+    FILE * dataBase = fopen("../data/sessao.txt", "r");
+    if (dataBase == NULL){
+        printf("### Nao foi possivel abrir o arquivo! ###\n");
+        exit(1);
+    }
+
+    while((c = fgetc(dataBase)) != EOF){
+        if (c == '\n'){
+            ++(*qtdSessoes);
         }
     }
-
+    fclose(dataBase);
 }
 
+
+Sessao * preencheListaSessoes(Sessao * sessoes, int * qtdSessao){
+    (*qtdSessao) = 0;
+    contaSessoes(qtdSessao);
+    int i = 0;
+    FILE * dataBase = fopen("../data/sessao.txt", "r");
+    if (dataBase == NULL){
+        printf("### Nao foi possivel abrir o arquivo! ###\n");
+        exit(1);
+    }
+    for(i = 0; i < (*qtdSessao); i++){
+        Sessao * aux  = (Sessao*) malloc(sizeof(Sessao));
+        fscanf(dataBase, " %d\t%[^\n]\n", &aux->id, aux->categoria);
+        sessoes = preencheNo(sessoes, aux);
+        free(aux);
+    }
+    fclose(dataBase);
+
+    return sessoes;
+}
+
+
+void imprimeSessao(Sessao * sessoes){
+    printf("As seguintes sessoes estao cadastradas:\n");
+    if(sessoes == NULL){
+        printf("A Sessao esta vazia");
+    } else {
+        Sessao* contador = sessoes->ant;
+        do {
+            printf("%d\t%s\n", contador->id, contador->categoria);
+            contador = contador->ant;
+        } while(contador != sessoes->ant);
+    }
+}

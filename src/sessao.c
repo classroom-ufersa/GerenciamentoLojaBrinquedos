a8347f076d71ea29f77da639c306c07e02e33032
euduar-da <mdasilvasouzs@gmail.com>
2024-04-07 18:44:14 -0300

    feat: add função para remover e adicionar sessoes e escrever no database

--- src/sessao.c
+++ src/sessao.c
@@ -59,7 +59,6 @@ void contaSessoes(int * qtdSessoes){
     fclose(dataBase);
 }
 
-
 Sessao * preencheListaSessoes(Sessao * sessoes, int * qtdSessao){
     (*qtdSessao) = 0;
     contaSessoes(qtdSessao);
@@ -80,7 +79,6 @@ Sessao * preencheListaSessoes(Sessao * sessoes, int * qtdSessao){
     return sessoes;
 }
 
-
 void imprimeSessao(Sessao * sessoes){
     printf("As seguintes sessoes estao cadastradas:\n");
     if(sessoes == NULL){
@@ -92,4 +90,66 @@ void imprimeSessao(Sessao * sessoes){
             contador = contador->ant;
         } while(contador != sessoes->ant);
     }
+}
+
+
+void imprimeDatabase(Sessao * sessoes){
+    Sessao* contador = sessoes->ant;
+    FILE * dataBase = fopen("../data/sessao.txt", "w");
+    if (dataBase == NULL){
+        printf("### Nao foi possivel abrir o arquivo! ###\n");
+        exit(1);
+    }
+    
+    do {
+        fprintf(dataBase, "%d\t%s\n", contador->id, contador->categoria);
+        contador = contador->ant;
+    } while(contador != sessoes->ant);
+    fclose(dataBase);
+}
+
+Sessao * adicionaSessao(Sessao * sessoes, int * qtdSessoes){
+    Sessao * aux = (Sessao*) malloc(sizeof(Sessao));
+    printf("Insira o nome da sessão: ");
+    scanf( "%[^\n]", aux->categoria);
+    aux->id = sessoes->id + 1;
+    sessoes = preencheNo(sessoes, aux);
+    ++(*qtdSessoes);
+    free(aux);
+    imprimeDatabase(sessoes);
+    return sessoes;
+}
+
+Sessao * removeSessao(Sessao * sessoes, int * qtdSessoes){
+    int idDesejado = 0;
+    printf("digite o id que deseja remover: ");
+    scanf("%d", &idDesejado);
+    Sessao* contador = sessoes;
+    do {
+        if(contador->id == idDesejado){
+            Sessao* proximo = contador->prox;
+            if(contador->ant != NULL) {
+                contador->ant->prox = contador->prox;
+            }
+            if(contador->prox != NULL) {
+                contador->prox->ant = contador->ant;
+            }
+            if(contador == sessoes) {
+                sessoes = sessoes->prox;
+            }
+            Sessao* anterior = contador->ant;
+            free(contador);
+            --(*qtdSessoes);
+            contador = anterior;
+            while(contador != sessoes->ant){
+                --(contador->id );
+                contador = contador->ant;
+            }
+            imprimeDatabase(sessoes);
+            return sessoes;
+        }
+        contador = contador->prox;
+    } while(contador != sessoes);
+    imprimeDatabase(sessoes);
+    return sessoes;
 }
